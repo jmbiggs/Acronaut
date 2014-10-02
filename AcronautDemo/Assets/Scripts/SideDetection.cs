779af5e37f6cd6e8d3e2e6bc40c721a040f94c0f
Michael Biggs <eraserhead@gmail.com>
2014-10-02 17:27:35 -0600

    Started working on wall cling

--- AcronautDemo/Assets/Scripts/SideDetection.cs
+++ AcronautDemo/Assets/Scripts/SideDetection.cs
@@ -1,17 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class FootDetection : MonoBehaviour {
+public class SideDetection : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D other)
 	{
+		print ("in");
 		GameObject player = GameObject.FindWithTag ("Player");
 		player.GetComponent<Player> ().grounded = true;
 	}
 	void OnTriggerExit2D (Collider2D other)
 	{
+		print ("out");
 		GameObject player = GameObject.FindWithTag ("Player");
 		player.GetComponent<Player> ().grounded = false;
 	}
-
 }
