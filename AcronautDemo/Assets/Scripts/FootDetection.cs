907b16516eac49a7e249329fa17e06257a4ee592
Michael Biggs <eraserhead@gmail.com>
2014-10-02 17:11:32 -0600

    Added ability to switch direction in midair, cleaned up code

--- AcronautDemo/Assets/Scripts/FootDetection.cs
+++ AcronautDemo/Assets/Scripts/FootDetection.cs
@@ -8,14 +8,12 @@ public class FootDetection : MonoBehaviour {
 		print ("in");
 		GameObject player = GameObject.FindWithTag ("Player");
 		player.GetComponent<Player> ().grounded = true;
-		print (player.GetComponent<Player> ().grounded);
 	}
 	void OnTriggerExit2D (Collider2D other)
 	{
 		print ("out");
 		GameObject player = GameObject.FindWithTag ("Player");
 		player.GetComponent<Player> ().grounded = false;
-		print (player.GetComponent<Player> ().grounded);
 	}
 
 }
--- AcronautDemo/Assets/Scripts/Player.cs
+++ AcronautDemo/Assets/Scripts/Player.cs
@@ -9,72 +9,73 @@ public class Player : MonoBehaviour {
 	public float jumpSpeed;
 	public float jumpMaxHeight;
 
-	private float airSpeedMultiplier = 1f; //.3f;
+	private float jumpOrigin; // the y position of player when a jump first begins
+	private bool jumpsOver = true;  // whether or not the jump has run its course
 
-	private float jumpOrigin;
-	private bool jumpsOver = true;
-
-	//private bool facingRight = true;
-
-
+	// ends any current jump
 	public void KillJump() {
 		jumpsOver = true;
 	}
 
 	// Update is called once per frame
 	void Update () {
+		// forces to be added to player's motion
 		var forceX = 0f;
 		var forceY = 0f;
 
-		var absVelX = Mathf.Abs (rigidbody2D.velocity.x);
-		var absVelY = Mathf.Abs (rigidbody2D.velocity.y);
-
-		// test whether player is airborne
-		if (absVelY < .2f) {
-			grounded = true;
-		} else {
-			grounded = false;
-		}
+		// current velocity of player
+		var velX = rigidbody2D.velocity.x;
+		var absVelX = Mathf.Abs (velX);
+		var velY = rigidbody2D.velocity.y;
+		var absVelY = Mathf.Abs (velY);
 
 		// get the player's (possible) left/right input
 		// it will be between -1 and 1
 		var horizInput = Input.GetAxis ("Horizontal") * speed;
 
-		if (absVelX < maxVelocity.x) {
-			// right direction
-			if (horizInput > 0){
-				forceX = grounded ? speed : (speed * airSpeedMultiplier);
+		// right direction
+		if (horizInput > 0){
+			if (velX < 0) // if heading left, switch directions
+			{
+				forceX = speed;
 				transform.localScale = new Vector3(1, 1, 1);
 			}
-			// left direction
-			else if (horizInput < 0){
-				forceX = grounded ? -speed : (-speed * airSpeedMultiplier);
+			else if (absVelX < maxVelocity.x) {
+				forceX = speed;
+				transform.localScale = new Vector3(1, 1, 1);
+			}
+		}
+		// left direction
+		else if (horizInput < 0){
+			if (velX > 0){ // if heading right, switch directions
+				forceX = -speed;
+				transform.localScale = new Vector3(-1, 1, 1);
+			}
+			else if (absVelX < maxVelocity.x){
+				forceX = -speed;
 				transform.localScale = new Vector3(-1, 1, 1);
 			}
 		}
 
+
 		// Handle the jump button
-		if (Input.GetButtonDown ("Jump") && grounded)
-			jumpsOver = false;
-
-		if (Input.GetButtonUp ("Jump"))
-			jumpsOver = true;
-
-		if (Input.GetButton ("Jump") && !jumpsOver) {
-			if (grounded) {
+		if (Input.GetButtonDown ("Jump") && grounded) {
 				jumpOrigin = transform.position.y;
 				jumpsOver = false;
-			} 
-
+		}
+		if (Input.GetButtonUp ("Jump"))
+			jumpsOver = true;
+		if (Input.GetButton ("Jump") && !jumpsOver) {
 			if (transform.position.y >= (jumpMaxHeight + jumpOrigin)) {
-				forceY = 0;
+				forceY = 0f;
 				jumpsOver = true;
 			} else {
-				if (!jumpsOver && absVelY < maxVelocity.y)
+				if (absVelY < maxVelocity.y)
 				forceY = jumpSpeed;
 			}
 		}
 
+		// apply the new force to the player
 		rigidbody2D.AddForce(new Vector2(forceX, forceY));
 	}
 }
