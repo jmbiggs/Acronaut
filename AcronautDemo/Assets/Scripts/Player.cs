3e85ae62555cf6777b80f7e19205058b4325fd81
Michael Biggs <eraserhead@gmail.com>
2014-10-04 19:18:45 -0600

    a little more tinkering

--- AcronautDemo/Assets/Scripts/Player.cs
+++ AcronautDemo/Assets/Scripts/Player.cs
@@ -4,30 +4,26 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
 	public float speed;
-	public Vector2 maxVelocity;
 	public bool grounded = true;
 	public float jumpSpeed;
 	public float jumpMaxHeight;
-	public float jumpHang;
+	public float hoverMax;
 	public float hoverMultiplier;
 	
 	private float jumpOrigin; // the y position of player when a jump first begins
 	private bool jumpIsOver = true;  // whether or not the jump has run its course
-	private float hangCount = 0;
 	private bool jumpDescending = false;
 	private float hoverCount; // TO ADD
 	
 	// ends any current jump
 	public void KillJump() {
 		jumpIsOver = true;
+		jumpDescending = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// forces to be added to player's motion
-		var forceY = 0f;
-		
-		
+
 		// get the player's (possible) left/right input
 		// it will be between -1 and 1
 		var horizInput = Input.GetAxis ("Horizontal") * speed;		
@@ -47,34 +43,33 @@ public class Player : MonoBehaviour {
 		
 		// Handle the jump button
 		if (Input.GetButtonDown ("Jump") && grounded) {
+			// initiate jump
 			jumpOrigin = transform.position.y;
 			jumpIsOver = false;
 		}
 		if (Input.GetButtonUp ("Jump"))
+			// immediately stop jump
 			jumpIsOver = true;
 		if (Input.GetButton ("Jump") && !jumpIsOver) {
 			float vertTranslation = Time.deltaTime * jumpSpeed;
 			if (jumpDescending) {
-				// descend
-				//print ("descent");
-				//vertTranslation *= hoverMultiplier;
-				//transform.Translate (0, -vertTranslation, 0);
+				// descending
+				hoverCount += Time.deltaTime;
+				if (hoverCount < hoverMax){
+					// slow down
+					rigidbody2D.AddForce(new Vector2(0, hoverMultiplier));
+				}
 			}
-			if (transform.position.y <= (jumpMaxHeight + jumpOrigin)) {
-				// ascend
-				print ("ascent");
+			else if (transform.position.y <= (jumpMaxHeight + jumpOrigin)) {
+				// ascending
+				//print ("ascent");
 				transform.Translate (0, vertTranslation, 0);
 			} else {
-				// hang
-				print ("hang");
-				hangCount += Time.deltaTime;
-				if (hangCount > jumpHang)
-					jumpDescending = true;
+				// start descent
+				jumpDescending = true;
+				hoverCount = 0;
 			}
 		}
-		
-		// apply the new force to the player
-		rigidbody2D.AddForce(new Vector2(0, forceY));
 	}
 	
 }
