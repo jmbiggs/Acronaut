794e3c529dfbd5c388583d80901cfcdd32dad42e
Michael Biggs <eraserhead@gmail.com>
2014-10-11 17:14:00 -0600

    tinkering with gravity and stuff
    not sure if I accomplished anything

--- AcronautDemo/Assets/Scripts/Player_rc.cs
+++ AcronautDemo/Assets/Scripts/Player_rc.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player_rc : MonoBehaviour {
 
 	public float speed;
 	public bool grounded;
@@ -16,22 +16,63 @@ public class Player : MonoBehaviour {
 
 	private float jumpOrigin; // the y position of player when a jump first begins
 	private bool jumpIsOver = true;  // whether or not the jump has run its course
-	//private bool falling = false;
+	private bool falling = false;
 	private float currentJump = 0f; // when in a jump
 	private float hoverCount; // TO ADD
+
+	private int horizRays = 6;
+	private int vertRays = 4;
+	private int margin = 2;
 
 	// ends any current jump
 	public void KillJump() {
 		jumpIsOver = true;
-		//falling = false;
+		falling = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		Rect box = new Rect (
+			collider.bounds.min.x,
+			collider.bounds.min.y,
+			collider.bounds.size.x,
+			collider.bounds.size.y
+		);
+
+		// reset translations
+		horizTranslation = 0f;
+		vertTranslation = 0f;
+
+		// detect collisions
+		float dist = 0.6f;
+		RaycastHit hitInfo;
+		Vector3 startPt = new Vector3 (box.xMin + margin, box.center.y, transform.position.z);
+		Vector3 endPt = new Vector3 (box.xMax - margin, box.center.y, transform.position.z);
+
+		bool hit = false;
+
+		for (int i=0; i < vertRays; i++) {		
+			float lerpAmount = (float)i / (float)vertRays - 1;
+			Vector3 origin = Vector3.Lerp(startPt, endPt, lerpAmount);
+			Ray ray = new Ray(origin, Vector3.down);
+
+			hit = Physics.Raycast(ray, out hitInfo, dist);
+
+			if (hit){
+				grounded = true;
+				falling = false;
+				break;
+			}
+		}
+		if (!hit)
+			grounded = false;
+
 		// apply gravity (if necessary)
 		if (!grounded) {
-			vertTranslation -= Time.deltaTime * gravity;
+			vertTranslation += Time.deltaTime - gravity;
+			if (vertTranslation < 0)
+				falling = true;
 		}
 
 		// get the player's (possible) left/right input
