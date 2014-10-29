cf60f2559216103c2bb0c69b9820b7fc338ef0ee
Adam Kay <adamkay@backcode.com>
2014-10-29 00:13:41 -0600

    Resized camera, didn't tweak any movement values

---
+++ AcronautDemo/Assets/ScreenSettings.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenSettings : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution(640, 360, true, 60);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
