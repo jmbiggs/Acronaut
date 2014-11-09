9c60822e9850ebf8543d30ca64e20f0b0b2ab596
Michael Biggs <eraserhead@gmail.com>
2014-11-08 20:13:59 -0700

    Put rhino in.  It's weird, but really needs the real artwork/colliders to get it right.

---
+++ AcronautDemo/Assets/Scripts/Rhino.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rhino : MonoBehaviour {
+
+	private PlayerController pc;
+	
+	public float bounceSpeed;
+	public float dashingBounceSpeed;
+
+	// Use this for initialization
+	void Start () {
+		pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Bounce up, speed depends on whether player is dashing
+	void OnCollisionEnter2D(Collision2D coll){
+		float newSpeed = pc.isDashing ? dashingBounceSpeed : bounceSpeed;
+		pc.vertVelocity = newSpeed;
+	}
+}
