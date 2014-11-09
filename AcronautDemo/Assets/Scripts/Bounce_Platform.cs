9276ae968cf20ac8a8d4aad6f30826636fbfd419
Michael Biggs <eraserhead@gmail.com>
2014-11-08 19:51:04 -0700

    Put bounce platform in. Again, not the best, but it's in.

---
+++ AcronautDemo/Assets/Scripts/Bounce_Platform.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bounce_Platform : MonoBehaviour {
+
+	private PlayerController pc;
+
+	public float bounceMultiplier;
+	
+	// Use this for initialization
+	void Start () {
+		pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Reverse the player's vertical velocity, and multiply it by the bounce multiplier
+	void OnCollisionEnter2D(Collision2D coll){
+		pc.SetGrounded ();
+		pc.vertVelocity *= bounceMultiplier * -1;
+	}
+}
