6a7261afb488bd2a61a4533fd16a117319461c9c
Michael Biggs <eraserhead@gmail.com>
2014-11-10 23:37:10 -0700

    put a rough hoop in

---
+++ AcronautDemo/Assets/Scripts/Hoop.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hoop : MonoBehaviour {
+	
+	private PlayerController pc;
+	
+	// Use this for initialization
+	void Start () {
+		pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		print ("oh hi");
+		pc.Knockback (-1);	
+	}
+}
