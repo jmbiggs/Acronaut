30cec1d6d9330667832130c6eca9dc74c2f484c8
Michael Biggs <eraserhead@gmail.com>
2014-11-12 16:34:32 -0700

    put tightrope in

---
+++ AcronautDemo/Assets/Scripts/Tightrope.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tightrope : MonoBehaviour {
+	
+	private PlayerController pc;
+	
+	public float slowdownMultiplier;
+	
+	// Use this for initialization
+	void Start () {
+		pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+	}
+
+	// Slows the player down
+	void OnCollisionEnter2D(Collision2D coll){
+		pc.speed *= slowdownMultiplier;
+	}
+
+	// Returns player to normal speeds
+	void OnCollisionExit2D(Collision2D coll){
+		pc.speed /= slowdownMultiplier;
+	}
+}
