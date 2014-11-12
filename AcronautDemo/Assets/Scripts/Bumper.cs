744bfa5a98df90ff90edfdd7575d243a42015b70
Michael Biggs <eraserhead@gmail.com>
2014-11-12 16:17:58 -0700

    put in bumper

--- AcronautDemo/Assets/Scripts/Bumper.cs
+++ AcronautDemo/Assets/Scripts/Bumper.cs
@@ -1,26 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bounce_Platform : MonoBehaviour {
-
+public class Bumper : MonoBehaviour {
+	
 	private PlayerController pc;
-
+	
 	public float bounceMultiplier;
 	
 	// Use this for initialization
 	void Start () {
 		pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
-	// Reverse the player's vertical velocity, and multiply it by the bounce multiplier
+	// Reverse the player's vertical and horizontal velocities
 	void OnCollisionEnter2D(Collision2D coll){
 		pc.RefreshAirMoves();
-		pc.gravityVelocity = 0f; // reset gravity, to get a little extra bounce
 		pc.vertVelocity *= bounceMultiplier * -1;
+		pc.horizVelocity *= bounceMultiplier * -1;
 	}
 }
