758113049f3f18c45bbbcd25ec4a0de185d24bc0
Adam Kay <adamkay@backcode.com>
2014-11-18 20:10:20 -0700

    class sync

---
+++ AcronautDemo/Assets/TutorialBox.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TutorialBox : MonoBehaviour {
+
+	public string textToDisplay;
+	Text textBox;
+
+	// Use this for initialization
+	void Start () {
+		textBox = GameObject.FindGameObjectWithTag("Tutorial").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		textBox.text = textToDisplay;
+		textBox.enabled = true;
+	}
+
+	void OnTriggerExit2D(Collider2D coll) {
+		textBox.enabled = false;
+	}
+}
